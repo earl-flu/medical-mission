35535fc422eca0b79e6825a22f32a06ba7ac3026
Earl John Sarmiento <earlsarmiento.dev@gmail.com>
2026-01-21 16:42:20 +0800

    update: added order button to the patient show page

--- resources/js/Components/EncounterRow.vue
+++ resources/js/Components/EncounterRow.vue
@@ -40,8 +40,13 @@ function deleteEncounter(encounter) {
     </td>
     <td class="px-6 py-4 text-right">
       <Link
-        :href="route('encounter.show', props.encounter)"
+        :href="route('order-items.create', props.encounter)"
         class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >Order</Link
+      >
+      <Link
+        :href="route('encounter.show', props.encounter)"
+        class="font-medium text-blue-600 dark:text-blue-500 hover:underline ml-3"
         >Show</Link
       >
       <Link
